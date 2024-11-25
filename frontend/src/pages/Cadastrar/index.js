bf1575ee72c414761d2fc48a666cc43285a5106d
carvalhoandre <andre_carvalho0@live.com>
2024-11-25 20:16:31 -0300

    fix: runnig rebot project

--- frontend/src/pages/Cadastrar/index.js
+++ frontend/src/pages/Cadastrar/index.js
@@ -1,166 +1,165 @@
-import { Form, Button, message, DatePicker, Layout, Menu, Input, InputNumber, Select } from "antd";
+import {
+  Form,
+  Button,
+  message,
+  DatePicker,
+  Layout,
+  Menu,
+  Input,
+  InputNumber,
+  Select,
+} from "antd";
 import { useEffect, useState } from "react";
 
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 import { retrieveAllCategoria, saveInvestimento } from "../../service/services";
 
 const { Header, Content, Footer } = Layout;
 const { Option } = Select;
 
 export default function Cadastrar() {
+  const [categorias, setCategorias] = useState([]);
+  const [categoria, setCategoria] = useState(null);
 
-    const [categorias, setCategorias] = useState([]);
-    const [categoria, setCategoria] = useState(null);
+  useEffect(() => {
+    refreshCategorias();
+    return () => {};
+  }, []);
 
-    useEffect(() => {
-        refreshCategorias();
-        return () => {
+  async function refreshCategorias() {
+    retrieveAllCategoria().then((response) => {
+      setCategorias(response.data);
+    });
+  }
 
-        }
-    }, [categorias, categoria])
+  const layout = {
+    labelCol: {
+      span: 4,
+    },
+    wrapperCol: {
+      span: 3,
+    },
+  };
+  const tailLayout = {
+    wrapperCol: {
+      offset: 4,
+    },
+  };
 
-    async function refreshCategorias() {
-        retrieveAllCategoria()
-            .then(
-                response => {
-                    setCategorias(response.data)
-                }
-            )
-    }
+  const onFinish = (values) => {
+    console.log(values);
+    saveInvestimento(values);
+    message.success("Inestimento salvo com sucesso");
+  };
 
-    const layout = {
-        labelCol: {
-            span: 4,
-        },
-        wrapperCol: {
-            span: 3,
-        },
-    };
-    const tailLayout = {
-        wrapperCol: {
-            offset: 4,
-        },
-    };
+  const onFinishFailed = (values) => {
+    message.danger("Falha ao cadastrar");
+    console.log("Failde: ", values);
+  };
 
-    const onFinish = (values) => {
-        console.log(values)
-        saveInvestimento(values)
-        message.success("Inestimento salvo com sucesso")
-    }
+  function handleChange(value) {
+    setCategoria(value);
+  }
 
-    const onFinishFailed = (values) => {
-        message.danger("Falha ao cadastrar")
-        console.log('Failde: ', values)
-    }
+  return (
+    <div className="contianer">
+      <Layout className="layout">
+        <Header>
+          <div className="logo" />
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["1"]}>
+            <Menu.Item key="1">
+              <Link to="/">Casatrar Investimentos</Link>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <Link to="listar">Listar Investimentos</Link>
+            </Menu.Item>
+          </Menu>
+        </Header>
+        <Content style={{ padding: "0 50px" }}>
+          <div className="site-layout-content">
+            <h2>CADASTRAR INVESTIMENTOS</h2>
+            <Form
+              {...layout}
+              name="basic"
+              initialValues={{
+                remenber: true,
+              }}
+              onFinish={onFinish}
+              onFinishFailed={onFinishFailed}
+            >
+              <Form.Item
+                label="Código do ativo"
+                name="active"
+                rules={[
+                  {
+                    required: true,
+                    message: "Insira o código do ativo!",
+                  },
+                ]}
+              >
+                <Input />
+              </Form.Item>
 
-    function handleChange(value) {
-        setCategoria(value)
-    }
+              <Form.Item
+                label="Valor"
+                name="value"
+                rules={[
+                  {
+                    required: true,
+                    message: "Insira o valor do investimento!",
+                  },
+                ]}
+              >
+                <Input />
+              </Form.Item>
+              <Form.Item
+                label="Quantidade de cotas"
+                name="quota"
+                rules={[
+                  {
+                    required: true,
+                    message: "Insira o valor da cota!",
+                  },
+                ]}
+              >
+                <InputNumber />
+              </Form.Item>
 
-    return (
-        <div className="contianer">
-            <Layout className="layout">
-                <Header>
-                    <div className="logo" />
-                    <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
-                        <Menu.Item key="1">
-                            <Link to="/">
-                                Casatrar Investimentos
-                                </Link>
-                        </Menu.Item>
-                        <Menu.Item key="2">
-                            <Link to="listar">
-                                Listar Investimentos
-                                </Link>
-                        </Menu.Item>
-                    </Menu>
-                </Header>
-                <Content style={{ padding: '0 50px' }}>
-                    <div className="site-layout-content">
-                        <h2>CADASTRAR INVESTIMENTOS</h2>
-                        <Form {...layout} name="basic"
-                            initialValues={{
-                                remenber: true,
-                            }}
-                            onFinish={onFinish}
-                            onFinishFailed={onFinishFailed}
-                        >
-                            <Form.Item
-                                label="Código do ativo"
-                                name="active"
-                                rules={[
-                                    {
-                                        required: true,
-                                        message: 'Insira o código do ativo!'
-                                    }
-                                ]}
-                            >
-                                <Input />
-                            </Form.Item>
+              <Form.Item
+                label="Data da Compra"
+                name="purchase"
+                rules={[
+                  {
+                    required: true,
+                    message: "Insira a data da compra!",
+                  },
+                ]}
+              >
+                <DatePicker />
+              </Form.Item>
 
-                            <Form.Item
-                                label="Valor"
-                                name="value"
-                                rules={[
-                                    {
-                                        required: true,
-                                        message: 'Insira o valor do investimento!'
-                                    }
-                                ]}
-                            >
-                                <Input />
-                            </Form.Item>
-                            <Form.Item
-                                label="Quantidade de cotas"
-                                name="quota"
-                                rules={[
-                                    {
-                                        required: true,
-                                        message: 'Insira o valor da cota!'
-                                    }
-                                ]}
-                            >
-                                <InputNumber />
-                            </Form.Item>
+              <Form.Item label="Categoria" name="category">
+                <Select style={{ width: "80%" }} onChange={handleChange}>
+                  {categorias.map((item, index) => {
+                    return (
+                      <Option key={item.id} value={index}>
+                        {item.name}
+                      </Option>
+                    );
+                  })}
+                </Select>
+              </Form.Item>
 
-                            <Form.Item
-                                label="Data da Compra"
-                                name="purchase"
-                                rules={[
-                                    {
-                                        required: true,
-                                        message: 'Insira a data da compra!'
-                                    },
-                                ]}
-                            >
-                                <DatePicker />
-                            </Form.Item>
-
-                            <Form.Item
-                                label="Categoria"
-                                name="category"
-                            >
-                                <Select style={{ width: '80%' }} onChange={handleChange}>
-                                    {categorias.map((item, index) => {
-                                        return (
-                                            <Option key={item.id} value={index} >
-                                                {item.name}
-                                            </Option>
-                                        )
-                                    })}
-                                </Select>
-                            </Form.Item>
-
-                            <Form.Item {...tailLayout}>
-                                <Button type="primary" htmlType="submit">
-                                    Salvar
-                                    </Button>
-                            </Form.Item>
-                        </Form>
-                    </div>
-                </Content>
-                <Footer style={{ textAlign: 'center' }}>© AC Invest 2021</Footer>
-            </Layout>
-        </div>
-    );
+              <Form.Item {...tailLayout}>
+                <Button type="primary" htmlType="submit">
+                  Salvar
+                </Button>
+              </Form.Item>
+            </Form>
+          </div>
+        </Content>
+        <Footer style={{ textAlign: "center" }}>© AC Invest 2021</Footer>
+      </Layout>
+    </div>
+  );
 }
--- frontend/src/pages/Listar/index.js
+++ frontend/src/pages/Listar/index.js
@@ -1,77 +1,81 @@
-import { useEffect, useState } from 'react'
-import "antd/dist/antd.css"
+import { useEffect, useState } from "react";
+import "antd/dist/antd.css";
 import { Link } from "react-router-dom";
 
-import { Table, Button, message, Layout, Menu } from 'antd'
+import { Table, Button, message, Layout, Menu } from "antd";
 
-import { retrieveAllInvestimento, deleteInvestimento } from '../../service/services';
-
+import {
+  retrieveAllInvestimento,
+  deleteInvestimento,
+} from "../../service/services";
 
 const { Header, Content, Footer } = Layout;
-const { Column } = Table
+const { Column } = Table;
 
 export default function Listar() {
+  const [investimento, setInvestimento] = useState([]);
 
-    const [investimento, setInvestimento] = useState([]);
+  useEffect(() => {
+    refreshInvestimento();
+    return () => {};
+  }, [investimento]);
 
-    useEffect(() => {
-        refreshInvestimento();
-        return () => {
+  async function refreshInvestimento() {
+    retrieveAllInvestimento().then((response) => {
+      console.log(response);
+      setInvestimento(response.data);
+    });
+  }
 
-        }
-    }, [investimento])
+  function remove(record) {
+    deleteInvestimento(record);
+    message.success("Investimento removido com sucesso");
+  }
 
-    async function refreshInvestimento(){
-        retrieveAllInvestimento()
-            .then(
-                response => {
-                    console.log(response)
-                    setInvestimento(response.data)
-                }
-            )
-    }
-
-    function remove(record) {
-        deleteInvestimento(record)
-        message.success('Investimento removido com sucesso')
-    }
-
-    return (
-        <div className="container">
-            <Layout className="layout">
-                <Header>
-                    <div className="logo" />
-                    <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-                        <Menu.Item key="1">
-                            <Link to="/">
-                                Cadastrar Investimento
-                                </Link>
-                        </Menu.Item>
-                        <Menu.Item key="2">
-                            <Link to="/listar">
-                                Listar Investimentos
-                                </Link>
-                        </Menu.Item>
-                    </Menu>
-                </Header>
-                <Content style={{ padding: '0 50px' }}>
-                    <div className="site-layout-content">
-                        <h2>INVESTIMENTOS</h2>
-                        <Table dataSource={investimento}>
-                            <Column title="Código do ativo" dataIndex="active" key="active" />
-                            <Column title="Valor" dataIndex="value" key="value" />
-                            <Column title="Quantidade de Cotas" dataIndex="quota" key="quota" />
-                            <Column title="Data da Compra" dataIndex="purchase" key="purchase" />
-                            <Column title="Remover" key="atualizar"
-                                render={(_text, record) => (<Button onClick={() => remove(record.id)}
-                                    type="primary">Remover</Button>)}
-                            />
-                        </Table>
-                    </div>
-                    
-                </Content>
-                <Footer style={{ textAlign: 'center' }}>© AC Invest 2021</Footer>
-            </Layout>
-        </div>
-    )
+  return (
+    <div className="container">
+      <Layout className="layout">
+        <Header>
+          <div className="logo" />
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
+            <Menu.Item key="1">
+              <Link to="/">Cadastrar Investimento</Link>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <Link to="/listar">Listar Investimentos</Link>
+            </Menu.Item>
+          </Menu>
+        </Header>
+        <Content style={{ padding: "0 50px" }}>
+          <div className="site-layout-content">
+            <h2>INVESTIMENTOS</h2>
+            <Table dataSource={investimento}>
+              <Column title="Código do ativo" dataIndex="active" key="active" />
+              <Column title="Valor" dataIndex="value" key="value" />
+              <Column
+                title="Quantidade de Cotas"
+                dataIndex="quota"
+                key="quota"
+              />
+              <Column
+                title="Data da Compra"
+                dataIndex="purchase"
+                key="purchase"
+              />
+              <Column
+                title="Remover"
+                key="atualizar"
+                render={(_text, record) => (
+                  <Button onClick={() => remove(record.id)} type="primary">
+                    Remover
+                  </Button>
+                )}
+              />
+            </Table>
+          </div>
+        </Content>
+        <Footer style={{ textAlign: "center" }}>© AC Invest 2021</Footer>
+      </Layout>
+    </div>
+  );
 }
